1e11ce62df62fec1be32c77d254cb09fa8cce213
BarryButtons <20088160@mail.wit.ie>
2023-03-02 21:04:18 +0000

    Local Commit

--- app/src/main/java/com/example/campaign/CampaignActivity.kt
+++ app/src/main/java/com/example/campaign/CampaignActivity.kt
@@ -2,10 +2,16 @@ package com.example.campaign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.i
 
 class CampaignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compaign)
+
+        Timber.plant(Timber.DebugTree())
+
+        i("Campaign Tracker has begun.....")
     }
 }
