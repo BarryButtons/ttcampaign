9094a63c47ae2cbde5177fc67834c4e6f8eea38e
BarryButtons <20088160@mail.wit.ie>
2023-03-03 00:09:08 +0000

    Move "Add Campaign Button"

--- app/src/main/java/com/example/campaign/CampaignActivity.kt
+++ app/src/main/java/com/example/campaign/CampaignActivity.kt
@@ -2,16 +2,25 @@ package com.example.campaign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.campaign.databinding.ActivityCampaignBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class CampaignActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCampaignBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_compaign)
+
+        binding = ActivityCampaignBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_campaign)
 
         Timber.plant(Timber.DebugTree())
 
         i("Campaign Tracker has begun.....")
+
+       binding.btnAdd.setOnClickListener() {
+            i("add Button Pressed")
+        }
     }
 }
